09f2410382f04ef970fcf92f35ce401315eeaf93
williammorris <wmorris421@gmail.com>
2017-02-14 16:36:34 +0000

    Added main file

---
+++ heatdistribution-DIPC/heatdistribution-DIPC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  heatdistribution-DIPC
+//
+//  Created by William Morris on 14/02/2017.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
