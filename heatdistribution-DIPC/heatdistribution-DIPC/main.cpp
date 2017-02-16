1a09b383179e499428b2be767f59626a505fe313
williammorris <wmorris421@gmail.com>
2017-02-16 18:45:05 +0000

    Added conditions for all boundries

--- heatdistribution-DIPC/heatdistribution-DIPC/main.cpp
+++ heatdistribution-DIPC/heatdistribution-DIPC/main.cpp
@@ -5,7 +5,6 @@
 //  Created by William Morris on 14/02/2017.
 //
 //
-
 #include <iostream>
 using namespace std;
 
@@ -28,25 +27,29 @@ int main() {
                 xyGrid[r][c]=100;
             }
             else {
-                xyGrid[r][c]=0;
+                //
+                if((r-1 == 0) && (c-1 == 0)){ //examine whether there is a boundry index at any given neighbouring points.
+                    //...//
+                } else if(r-1==0) {
+                    //..//
+                } else if (c-1==0){
+                    //..//
+                } else if((r+1==rowInputY)&&(c+1==columnInputX)){
+                    //..//
+                } else if (r+1==rowInputY){
+                    //..//
+                } else if (c+1==columnInputX){
+                    //..//
+                }
             }
             cout<< "    [" << xyGrid[r][c] << "]";
         }
         std::cout << endl;
     }//end for
     
-    
-    int c,r; //Declare inner looping variables outside the array for explicity when parallel processing
-    
-    
-//    n = 5;                 // we'll do the 5 x 5 case as an example
-//    vars = (n-2)^2;        // an n x n grid gives (n-2)^2 variables
-//    T = zeros(n)           // set up an n x n zero matrix
-//    T(1,:) = 1.0           // set all temperatures in row 1 to be 1.0
-//    T(:,1) = 1.0           // set all temperatures in column 1 to be 1.0
+    //Put values into a grid.
     
     
-
 //    for(r=0; r<rowInputY; r++){
 //        for(c=0;c<columnInputX;c++){
 //            double w = (0 + r-1 + c-1 + 100) * 0.25;
