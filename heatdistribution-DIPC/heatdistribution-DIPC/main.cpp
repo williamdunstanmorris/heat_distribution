907b885865ea0947c092f4a1f3cef7be64adfd24
williammorris <wmorris421@gmail.com>
2017-02-16 21:11:37 +0000

    fixed all boundry conditions in prep for calculation

--- heatdistribution-DIPC/heatdistribution-DIPC/main.cpp
+++ heatdistribution-DIPC/heatdistribution-DIPC/main.cpp
@@ -14,9 +14,9 @@ int main() {
     const int rowInputY = 10;
     double xyGrid[columnInputX][rowInputY]; //Declare an empty 2D array with size determined by use
     
-    
     for(int r = 0; r < rowInputY; r++){
         for(int c = 0; c <columnInputX; c++){
+            //fill all boundry indices with temperature values.
             if (r == 0) {
                 xyGrid[r][c]=0;
             }
@@ -25,27 +25,39 @@ int main() {
             }
             else if(r == rowInputY -1 ){
                 xyGrid[r][c]=100;
-            }
+            }//end boundry fill
             else {
-                //
-                if((r-1 == 0) && (c-1 == 0)){ //examine whether there is a boundry index at any given neighbouring points.
-                    //...//
-                } else if(r-1==0) {
-                    //..//
-                } else if (c-1==0){
-                    //..//
-                } else if((r+1==rowInputY)&&(c+1==columnInputX)){
-                    //..//
-                } else if (r+1==rowInputY){
-                    //..//
+                //fill all inner nodes with 0s.
+                xyGrid[r][c]=0;
+                
+                //Discrete heat calculation:
+                //examine whether there is a boundry index at any given neighbouring points.
+                if((r-1==0)&&(c-1==0)){
+                    //a:..//
+                } else if((r-1==0)&&(c+1==rowInputY)) {
+                    //b:..//
+                } else if ((r+1==rowInputY)&&(c+1==columnInputX)){
+                    //c:..//
+                } else if((r+1==rowInputY)&&(c-1==0)){
+                    //d:..//
                 } else if (c+1==columnInputX){
-                    //..//
+                    //e:..//
+                } else if (c-1==0){
+                    //f:..//
+                } else if (r-1==0) {
+                    //g:..//
+                } else if (r+1==rowInputY){
+                    //h:..//
+                } else if ((r-1==0)&&(r+1==rowInputY)&&(c-1==0)&&(c+1==columnInputX)){
+                    //i:..//
+                } else {
+                    //j..//
                 }
             }
             cout<< "    [" << xyGrid[r][c] << "]";
-        }
+        }//end for[column]
         std::cout << endl;
-    }//end for
+    }//end for[row]
     
     //Put values into a grid.
     
