5b8b70c4f7eaaeef13a463a5bc2577a24ff1de1b
williammorris <wmorris421@gmail.com>
2017-03-04 13:55:43 +0000

    Added resource files

--- heatDistribution/heatDistribution/main.cpp
+++ heatDistribution/heatDistribution/main.cpp
@@ -9,47 +9,52 @@ using namespace std;
 //------------------------------------------
 const int columnInputX = 3;
 const int rowInputY = 3;
+int i;
 
 double xyGrid[columnInputX][rowInputY]; //Declare an empty 2D array with size determined by use^I
 
-double difference;
-double totalDifference = 0;
-double stopCond = 1;
+double difference, addedValues, curTemp, sum;
+double accumDifference, storedDiff = 0;
+double diff = 1;
 double threshold = 0.01;
-double ultimateVal;
+double dynamicRange;
 
 double r[columnInputX][rowInputY] ;
 double g[columnInputX][rowInputY] ;
 double b[columnInputX][rowInputY] ;
 
+//------------------------------------------
+/*
+ *Function Declarations:
+ */
 int main(int argc, const char * argv[]);
-void setup();
-void hDOnePro();
+void init(const int row, const int column);
+void hDOnePro(const int row, const int column);
 void hDFourPro();
-void printGraphic();
-void calculateRGB();
+void writePPM(ofstream img);
+void calculateRGB(const int row, const int column);
 
 
 //------------------------------------------
 /*
  *Setup:
  */
-void setup(){
-    for(int c = 0; c < columnInputX; c++){
-        for(int r = 0; r <rowInputY; r++){
+void init(const int row, const int column){
+    for(int r = 0; r < row; r++){
+        for(int c = 0; c <column; c++){
             //Fill all boundary indices with temperature values.
-            if (c == 0) {
-                xyGrid[c][r]=0;
+            if (r == 0) {
+                xyGrid[r][c]=0;
             }
-            else if(r == 0 || r == rowInputY -1) {
-                xyGrid[c][r]=100;
+            else if(c == 0 || c == column-1 ) {
+                xyGrid[r][c]=100;
             }
-            else if(c == columnInputX -1 ){
-                xyGrid[c][r]=100;
+            else if(r == row-1 ){
+                xyGrid[r][c]=100;
             }//end boundary fill
             else {
                 //fill all inner nodes with 0s.
-                xyGrid[c][r]=0;
+                xyGrid[r][c]=0;
             }
             cout<< "    [" << xyGrid[r][c] << "]";
         }//end for[column]
@@ -59,63 +64,69 @@ void setup(){
 
 //------------------------------------------
 /*
- * One Processor:
+ * Serial Program:
  */
-void hDOnePro(){
+void hDOnePro(const int row, const int column){
 
+    //TODO: redo iteration variable names.
     do {
 //        ic++;
 //        cout<< "//--------------\nIteration [" << ic << "]\n" << endl;
-        for(int r = 0; r < rowInputY; r++){
-            for(int c = 0; c <columnInputX; c++){
+        for(int r = 0; r < row; r++){
+            for(int c = 0; c <column; c++){
 
                 //Reference the temperature for comparison later:
-                double curTemp = xyGrid[r][c];
+                curTemp = xyGrid[r][c];
 
                 //Calculate values inside boundary:
                 if((r != 0) && (r != rowInputY-1)) {
                     if((c != 0) && (c != columnInputX-1)){
 //                        std::cout <<"Index Point: [" << r << "][" << c << "] @W (temperature) = " << xyGrid[r][c] << endl;
-                        double addedValues = ((xyGrid[r-1][c])+
+                        //Equation split up into a 'divide and conquer' algorithm
+                        addedValues = ((xyGrid[r-1][c])+
                                 (xyGrid[r][c+1])+
                                 (xyGrid[r+1][c])+
                                 (xyGrid[r][c-1])); //Steady state heat equation.
-                        std::cout << "@addedValues = " << "(" << xyGrid[r-1][c]<< "+" << xyGrid[r][c+1]<< "+" << xyGrid[r+1][c]<< "+" << xyGrid[r][c-1]<< ") ="<< addedValues  <<endl;
+//                        std::cout << "@addedValues = " << "(" << xyGrid[r-1][c]<< "+" << xyGrid[r][c+1]<< "+" << xyGrid[r+1][c]<< "+" << xyGrid[r][c-1]<< ") ="<< addedValues  <<endl;
                         //@test(1) for corrent added values;
-                        double meanCalculation = (0.25)*addedValues;
 
                         //Successive estimate calculation: i.e the @new curTemp - current curTemp;
-                        std::cout << "@meanCalculation = "<< meanCalculation<< endl;
+//                        std::cout << "@sum = "<< sum<< endl;
 
-                        xyGrid[r][c] = meanCalculation;
+                        sum = (0.25)*addedValues;
+
+                        xyGrid[r][c] = sum;
 
                         difference = xyGrid[r][c] - curTemp;
-                        std::cout<<"@difference = " << xyGrid[r][c] << " - " << curTemp << " = " << difference << endl;
+//                        std::cout<<"@difference = " << xyGrid[r][c] << " - " << curTemp << " = " << difference << endl;
 
-                        totalDifference+=difference;
-                        std::cout<<"@totalAllDifference = " << totalDifference << "\n\n"<< endl;
+                        //Accumulative difference
+                        accumDifference+=difference;
+//                        std::cout<<"@totalAllDifference = " << accumDifference << "\n\n"<< endl;
 
                     }
                 }
             } // End Column
         } // End Row
 
-        double differenceLastGrid = stopCond;
+        //Store last difference calculation into a temporary variable
+        storedDiff = diff;
 
-        stopCond = totalDifference/(columnInputX*rowInputY);
-//        std::cout<<"@stopCondition = " << stopCond<< "\n\n"<<endl;
+        //Calculation of difference
+        diff = accumDifference/(columnInputX*rowInputY);
+//        std::cout<<"@stopCondition = " << diff<< "\n\n"<<endl;
+        dynamicRange = diff - storedDiff;
+        std::cout<<"@differencePoint = "<< dynamicRange << endl;
 
-        ultimateVal = stopCond - differenceLastGrid;
-//        std::cout<<"@differencePoint = "<< ultimateVal << endl;
+    } while(dynamicRange >= threshold);
 
-    } while(ultimateVal >= threshold);
+    calculateRGB(row, column);
 
-    calculateRGB();
 }
 
-void calculateRGB(){
-    for (int j = 0; j < columnInputX; j++) {
-        for (int i = 0; i < rowInputY; i++) {
+void calculateRGB(const int row, const int column){
+    for (int j = 0; j < row; j++) {
+        for (int i = 0; i < column; i++) {
             double f = xyGrid[j][i]/100; //Get value between 0:1
 
             double a=(1-f)/0.25;	//invert and group
@@ -141,13 +152,12 @@ void calculateRGB(){
             }
         }
     }
-
 }
 
-void printGraphic(ofstream img){
+void writePPM(){
 
     //TODO: Make a file that does appends and does not overwrite, and iterate over the file number
-
+    ofstream img("tryout.ppm");
     img << "P3" << endl;
     img << columnInputX << " " << rowInputY << endl;
     img << "255" << endl;
@@ -165,46 +175,16 @@ void printGraphic(ofstream img){
     system("open picture.ppm");
 }
 
-void runallOneProc(){
-    for(int i = 0; i < 10; i++){
-        if(i == 0) {
-            //TODO: Why can I not change the size of the array i.e. why does it have to be constant?
-            columnInputX = 10;
-            rowInputY = 10;
-            hDOnePro();
-            ofstream img("heatDistribution#1_10x10.ppm");
-            //TODO: Why this this call incorrect?
-            printGraphic(img);
-        } else if(i == 1){
-
-        } else if(i==2){
-
-        } else if (i==3){
-
-        } else if (i==4){
-
-        } else if (i==5){
-
-        }else if (i==6){
-
-        }else if (i==7){
-
-        }else if (i==8){
-
-        }else{
-
-        }
-    }
-
-}
-
-
 int main(int argc, const char * argv[]) {
 
     //TODO: Decide how to determine a variable tolerence
     //TODO: Make the col and row ints small and run it on this computer.
-    setup();
-    hDOnePro();
+    int r = 10; int c = 10;
+    init(r,c);
+    hDOnePro(r,c);
+    ofstream img("tryout.ppm");
+    writePPM();
+
     return 0;
 }
 
