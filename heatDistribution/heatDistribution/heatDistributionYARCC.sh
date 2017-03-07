3c58149243f4115dffd6b21346fa9b95f4e4d7a5
williammorris <wmorris421@gmail.com>
2017-03-07 20:09:49 +0000

    FEDIT

---
+++ heatDistribution/heatDistribution/heatDistributionYARCC.sh
@@ -0,0 +1,6 @@
+#$ -cwd
+#$ -l_h_rt=00:01:00
+#$ -l_hvmem=8GB
+#$ -l h_stack=memory
+#$ -pe smp 4
+#$ ./a.out
--- heatDistribution/heatDistribution/main.cpp
+++ heatDistribution/heatDistribution/main.cpp
@@ -93,7 +93,7 @@ void hDOnePro(std::vector< std::vector<double> >& vec,const double &threshold, s
   /*File .txt output in format:
    * #FileNumber_MxN_threshold
    * */
-  std::basic_ofstream txtFile(fname + ".txt");
+  ofstream txtFile(fname + ".txt");
 
   //Calculation parameters
   double difference, addedValues, curTemp;
